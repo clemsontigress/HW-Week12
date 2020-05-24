e2d0d950ffc5011c4be5690373481cc8a9ac02ef
clemson tigress <anreeves83@gmail.com>
2020-05-24 18:15:15 -0400

    set up schema and connection

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE database employeeTrackerDB;
+
+USE employeeTrackerDB;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT,,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
